850bce3f654c606d15af365c28e8dee06f14118f
MattyG <meawoppl@gmail.com>
2014-04-30 22:57:52 -0700

    Added a (probably wrong) setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyMeteor',
+      version='0.1',
+      description='Python tools for interacting with the Meteor web framework.',
+      author='Matthew Goodman and Ted Blackman',
+      author_email='software@3scan.com',
+      url='https://github.com/3Scan/pyMeteor',
+      py_modules=['pyDDP'],
+      )
