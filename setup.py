27da22a831ddfc934f1de83da202351f4b255f6e
Ted Blackman <ted.blackman@gmail.com>
2014-06-12 17:46:49 -0700

    Made version a SemVer.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pyMeteor',
-      version='0.1',
+      version='0.0.1',
       description='Python tools for interacting with the Meteor web framework.',
       author='Matthew Goodman and Ted Blackman',
       author_email='software@3scan.com',
